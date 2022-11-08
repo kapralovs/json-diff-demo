bd8462ae35ef19c27fc76e1bd490df645416882d
Sergey Kapralov <ser-kapralov@yandex.ru>
2022-11-08 08:07:27 +0300

    Fix bug that occures with replacing elemnts

--- main.go
+++ main.go
@@ -71,37 +71,24 @@ func main() {
 		if op.Type == "replace" || op.Type == "remove" {
 			continue
 		}
-		fmt.Printf("Before operation: %v\n", before)
-		fmt.Printf("Before operation: %v\n", after)
 		pathParts := strings.Split(op.Path.String()[1:], "/")
 		switch op.Type {
 		case "test":
-			fmt.Println("TEST")
 			testTypeCase(pathParts, op, before, after)
 			if patch[idx+1].Type == "remove" {
-				fmt.Println("REMOVE")
 				nextPathParts := strings.Split(patch[idx+1].String()[1:], "/")
 				removeTypeCase(nextPathParts, patch[idx+1], after)
-				fmt.Printf("After operation: %v\n", before)
-				fmt.Printf("After operation: %v\n", after)
 				continue
 			}
 			if patch[idx+1].Type == "replace" {
-				fmt.Println("REPLACE")
 				nextPathParts := strings.Split(patch[idx+1].Path.String()[1:], "/")
-				replaceTypeCase(op.Value, nextPathParts, patch[idx+1], after)
-				fmt.Printf("After operation: %v\n", before)
-				fmt.Printf("After operation: %v\n", after)
+				replaceTypeCase(op.Value, nextPathParts, patch[idx+1], before, after)
+
 				continue
 			}
 			after[patch[idx+1].Path.String()[1:]] = patch[idx+1].Value
-			fmt.Printf("After operation: %v\n", before)
-			fmt.Printf("After operation: %v\n", after)
 		case "add":
-			fmt.Println("ADD")
 			addTypeCase(idx, pathParts, op, after)
-			fmt.Printf("After operation: %v\n", before)
-			fmt.Printf("After operation: %v\n", after)
 		}
 	}
 
@@ -113,9 +100,10 @@ func main() {
 func testTypeCase(pathParts []string, op jsondiff.Operation, before, after map[string]interface{}) {
 	if len(pathParts) > 1 {
 		if _, ok := before[pathParts[0]]; !ok {
-			items := []interface{}{op.Value}
-			before[pathParts[0]] = items
-			after[pathParts[0]] = items
+			itemsBefore := []interface{}{op.Value}
+			itemsAfter := []interface{}{op.Value}
+			before[pathParts[0]] = itemsBefore
+			after[pathParts[0]] = itemsAfter
 			return
 		}
 		if values, ok := before[pathParts[0]].([]interface{}); ok {
@@ -161,14 +149,13 @@ func addTypeCase(idx int, pathParts []string, op jsondiff.Operation, after map[s
 	after[pathParts[0]] = op.Value
 }
 
-func replaceTypeCase(oldValue interface{}, pathParts []string, op jsondiff.Operation, after map[string]interface{}) {
+func replaceTypeCase(oldValue interface{}, pathParts []string, op jsondiff.Operation, before, after map[string]interface{}) {
 	if len(pathParts) > 1 {
 		if _, ok := after[pathParts[0]]; !ok {
 			after[pathParts[0]] = []interface{}{op.Value}
 			return
 		}
 		if values, ok := after[pathParts[0]].([]interface{}); ok {
-			fmt.Println("values", len(values))
 			for i := range values {
 				if values[i] == oldValue {
 					values[i] = op.Value

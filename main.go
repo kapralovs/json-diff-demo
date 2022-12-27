484a1efb82ccab0c7a4c39a5484b5335762a1700
Sergey Kapralov <ser-kapralov@yandex.ru>
2022-12-27 10:11:12 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 )
 
 type User struct {
-	Name     string   `json:"name,omitempty"`
-	Age      int      `json:"age,omitempty"`
-	IsAdult  bool     `json:"is_adult"`
-	Bp       Backpack `json:"bp,omitempty"`
-	Items    []string `json:"items,omitempty"`
-	Vehicles []Car    `json:"vehicles,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Age     int    `json:"age,omitempty"`
+	IsAdult bool   `json:"is_adult"`
+	// Bp       Backpack `json:"bp,omitempty"`
+	// Items    []string `json:"items,omitempty"`
+	// Vehicles []Car    `json:"vehicles,omitempty"`
 }
 
 type Backpack struct {
@@ -33,48 +33,106 @@ type Event struct {
 }
 
 func main() {
-	u1 := &User{Name: "John", Age: 27, IsAdult: true,
-		Bp:       Backpack{IsFull: false},
-		Items:    []string{"drink", "smartphone", "bubblegum"},
-		Vehicles: []Car{{Vendor: "VW", Model: "Polo"}},
+	testNames := []string{
+		"Sam",
+		"Serj",
+		"Aaron",
+		"Henry",
+		"Steven",
+		"Jackie",
+		"Alex",
+		"Johan",
+		"Bjorn",
+		"Anders",
 	}
-	u2 := &User{Name: "John", Age: 28, IsAdult: false,
-		Bp:    Backpack{Notebook: "Macbook", IsFull: true},
-		Items: []string{"smartphone", "bubblegum"},
-		Vehicles: []Car{
-			{Vendor: "VW", Model: "Polo"},
-			{Vendor: "Ford", Model: "Transit"},
-		},
+	updtPatches := make([]jsondiff.Patch, 0)
+	rbPatches := make([]jsondiff.Patch, 0)
+	u1 := &User{Name: "John", Age: 27, IsAdult: true} // Bp:       Backpack{IsFull: false},
+	// Items:    []string{"drink", "smartphone", "bubblegum"},
+	// Vehicles: []Car{{Vendor: "VW", Model: "Polo"}},
+
+	u2 := *u1
+
+	// u1Serialized, _ := json.Marshal(u1)
+
+	for idx, name := range testNames {
+		if idx == 3 {
+			u2.Age += 3
+		}
+		if idx == 6 {
+			u2.IsAdult = false
+		}
+		u2.Name = name
+		u1Serialized, _ := json.Marshal(u1)
+		u2Serialized, _ := json.Marshal(&u2)
+		updatePatch, err := jsondiff.CompareJSONOpts(u1Serialized, u2Serialized, jsondiff.Invertible())
+		if err != nil {
+			panic(err)
+		}
+		rollbackPatch, err := jsondiff.CompareJSONOpts(u2Serialized, u1Serialized, jsondiff.Invertible())
+		if err != nil {
+			panic(err)
+		}
+		if idx == 3 {
+			u1.Age += 3
+		}
+		if idx == 6 {
+			u1.IsAdult = false
+		}
+		u1.Name = name
+		fmt.Printf("ROLLBACK PATCH %d:\n", idx)
+		rbPatches = append(rbPatches, rollbackPatch)
+		fmt.Println(rollbackPatch.String())
+		fmt.Printf("UPDATE PATCH %d:\n", idx)
+		updtPatches = append(updtPatches, updatePatch)
+		fmt.Println(updatePatch.String())
+		fmt.Println()
 	}
 
-	u1Serialized, _ := json.Marshal(u1)
-	u2Serialized, _ := json.Marshal(u2)
+	u2Ptr := &u2
 
-	updatePatch, err := jsondiff.CompareJSONOpts(u1Serialized, u2Serialized, jsondiff.Invertible())
-	if err != nil {
-		panic(err)
+	fmt.Println("ROLLBACK:")
+	for i := len(rbPatches) - 1; i >= 0; i-- {
+		fmt.Println(i)
+		u2Serialized, _ := json.Marshal(u2Ptr)
+		// fmt.Println(string(u2Serialized))
+		patched, err := applyPatch(u2Serialized, rbPatches[i])
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = json.Unmarshal(patched, u2Ptr)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(patched))
 	}
-	rollbackPatch, err := jsondiff.CompareJSONOpts(u2Serialized, u1Serialized, jsondiff.Invertible())
-	if err != nil {
-		panic(err)
-	}
-
-	updated := applyPatch(u1Serialized, updatePatch)
-	rollbacked := applyPatch(updated, rollbackPatch)
-
-	fmt.Println(updatePatch.String())
 	fmt.Println()
-	fmt.Println(updatePatch.String())
-	fmt.Printf("Original document: %s\n", u1Serialized)
-	fmt.Printf("Original document2: %s\n", u2Serialized)
-	fmt.Printf("After Update: %s\n", updated)
-	fmt.Printf("After Rollback: %s\n", rollbacked)
+	fmt.Println("UPDATE:")
+	for i, patch := range updtPatches {
+		fmt.Println(i)
+		u2Serialized, _ := json.Marshal(u2Ptr)
+		// fmt.Println(string(u2Serialized))
+		patched, err := applyPatch(u2Serialized, patch)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = json.Unmarshal(patched, u2Ptr)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(patched))
+	}
 }
 
-func applyPatch(entity []byte, patch jsondiff.Patch) []byte {
+func applyPatch(entity []byte, patch jsondiff.Patch) ([]byte, error) {
 	patchSerialized, _ := json.Marshal(patch)
-	// fmt.Printf("PATCH DATA: %v\n", string(patchSerialized))
-	p, _ := jsonpatch.DecodePatch(patchSerialized)
-	patched, _ := p.Apply(entity)
-	return patched
+	p, err := jsonpatch.DecodePatch(patchSerialized)
+	if err != nil {
+		return nil, err
+	}
+	patched, err := p.Apply(entity)
+	if err != nil {
+		return nil, err
+	}
+	return patched, err
 }
